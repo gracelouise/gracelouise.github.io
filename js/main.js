d8975710245912ec96d135692500301f06a2eddb
gracelouise <grace.edinger@gmail.com>
2015-12-02 16:20:07 -0500

    updated insta and added responsive

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
-$('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
-    }, 500);
-    return false;
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 120
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
 
 $.ajax({
       type: "GET",
       dataType: "jsonp",
       cache: false,
-      url: "https://api.instagram.com/v1/users/self/media/recent/?access_token=15912969.af411b8.390acad5419c4d2ca6c130dfa092a5d2",
+      url: "https://api.instagram.com/v1/users/self/media/recent/?access_token=1228652019.36b37c9.50acd7b580fe473d8421d0b559898898",
       success: function(data) {
         for (var i = 0; i < 4; i++) {
       $("#photos").append("<li><a target='_blank' href='" + data.data[i].link + "'><img class='insta' src='" + data.data[i].images.low_resolution.url +"'></img></a></li>");
